3e1672b1306f12c7f6b2a1c1d7cf434469759e39
kpagratis <kostas@pagratis.us>
2023-01-25 08:11:36 -0700

    Migrate to notify-rs and general cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 use std::{thread, time};
 
-use rust_runtime_toggles::Toggles;
+use rust_runtime_toggles::{Toggle};
 
 fn main() {
-    // Statements here are executed when the compiled binary is called
-
-    // Print text to the console
-    println!("Hello World!");
-    let toggles = Toggles::new("toggle.yaml");
-
-    let _thread = Toggles::start(&toggles);
-
+    let notify_toggles = Toggle::new("toggle.yaml");
+    Toggle::start(&notify_toggles);
     loop {
         thread::sleep(time::Duration::from_secs(1));
-        println!("{}", toggles.is_available("halfOn"));
+        println!("{}", notify_toggles.is_available("halfOn"));
     }
 }
